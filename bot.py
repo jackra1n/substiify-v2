461c28c04522fce9a035c46f611789e860095586
jackra1n <lok7ok@gmail.com>
2021-09-23 19:11:49 +0200

    fix avatar slash command

--- bot.py
+++ bot.py
@@ -1,57 +1,53 @@
 import os
-import hikari
-import lightbulb
 import logging
 import json
+from pathlib import Path
+
+import hikari
+import lightbulb
 
 from utils import util, store
 
-if os.name != "nt":
-    import uvloop
 
-    uvloop.install()
+def create_bot() -> lightbulb.Bot:
+    util.prepareFiles()
 
-util.prepareFiles()
+    logging.getLogger("lightbulb").setLevel(logging.DEBUG)
+    _LOGGER = logging.getLogger(__name__)
 
-logging.getLogger("lightbulb").setLevel(logging.DEBUG)
-_LOGGER = logging.getLogger(__name__)
+    # Gather all slash command files.
+    extensions = Path("./extensions").glob("*.py")
 
-extensions = [
-    # "gif",
-    # "music",
-    # "duel",
-    # "daydeal",
-    # "epicGames",
-    # "util",
-    # "giveaway",
-    # "fun",
-    # "help",
-    "util"
-]
+    with open(store.settings_path, "r") as settings_file:
+        settings = json.load(settings_file)
 
+    if not settings["token"]:
+        _LOGGER.error(
+            f"No token in {store.settings_path}! Please add it and try again."
+        )
+        exit()
 
-with open(store.settings_path, "r") as settings_file:
-    settings = json.load(settings_file)
+    prefix = "<<"
 
-if not settings["token"]:
-    _LOGGER.error(f"No token in {store.settings_path}! Please add it and try again.")
-    exit()
-
-prefix = "<<"
-
-
-def start_bot():
     bot = lightbulb.Bot(
         prefix=prefix,
         token=settings["token"],
         intents=hikari.Intents.ALL,
     )
-    if len(extensions) != 0:
-        for ext in extensions:
-            bot.load_extension(f"extensions.{ext}")
-            _LOGGER.info(f"Loaded extension: {ext}")
-    bot.run()
+
+    for ext in extensions:
+        bot.load_extension(f"extensions.{ext}")
+        _LOGGER.info(f"Loaded extension: {ext}")
+
+    return bot
 
 
 if __name__ == "__main__":
-    start_bot()
+    if os.name != "nt":
+        # uvloop is only available on UNIX systems, but instead of coding
+        # for the OS, we include this if statement to make life easier.
+        import uvloop
+
+        uvloop.install()
+
+    create_bot().run()
--- extensions/util.py
+++ extensions/util.py
@@ -7,23 +7,24 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
+
 class Avatar(slash_commands.SlashCommand):
     description = "Shows bigger picture of an avatar of a user"
     # Options
-    enabled_guilds: typing.Optional[typing.Iterable[int]] = (742698112081985588,)
     user: typing.Optional[User] = slash_commands.Option("User")
 
-    async def callback(self, context):
-        user = context.author
-        if context.options["user"]:
-            user = context.options["user"].value
+    async def callback(self, ctx):
+        user: User = ctx.member
+        if ctx.option_values.user:
+            user = ctx.get_guild().get_member(int(ctx.option_values.user))
         embed = embeds.Embed(
             title=str(user.display_name),
             description="Avatar",
             colour=Color(0x00BBFF),
         )
         embed.set_image(user.avatar_url)
-        await context.respond(embed=embed)
+        await ctx.respond(embed=embed)
+
 
 def load(bot):
     bot.add_slash_command(Avatar)
