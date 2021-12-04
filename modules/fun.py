9073f9aa3611926efb9b2f7842a2e4b49a3664e3
jackra1n <lok7ok@gmail.com>
2021-12-04 21:52:57 +0100

    Bring back 8ball

---
+++ modules/fun.py
@@ -0,0 +1,46 @@
+import logging
+import random
+
+import nextcord
+from nextcord.ext import commands
+
+logger = logging.getLogger(__name__)
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.cooldown(6, 5)
+    @commands.command(aliases=['8ball'], brief='AKA 8ball, Ask the bot a question that you dont want the answer to.')
+    async def eightball(self, ctx,*,question):
+        responses = ["It is certain.",
+                    "It is decidedly so.",
+                    "Without a doubt.",
+                    "Yes - definitely.",
+                    "You may rely on it.",
+                    "As I see it, yes.",
+                    "Most likely.",
+                    "Outlook good.",
+                    "Yes.",
+                    "Signs point to yes.",
+                    "Reply hazy, try again.",
+                    "Ask again later.",
+                    "Better not tell you now.",
+                    "Cannot predict now.",
+                    "Concentrate and ask again.",
+                    "Don't count on it.",
+                    "My reply is no.",
+                    "My sources say no.",
+                    "Outlook not so good.",
+                    "Very doubtful."]
+        response = random.choice(responses)
+        embed = nextcord.Embed(
+            title = response,
+            description=f'Question: {question}',
+            colour = nextcord.Colour.orange()
+        )
+        embed.set_footer(text=f'Question by {ctx.author}', icon_url=ctx.author.avatar.url)
+        await ctx.channel.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Fun(bot))
