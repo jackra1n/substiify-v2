c0412b095a3357ba7193e71483cd76ed07e54ca0
jackra1n <lok7ok@gmail.com>
2021-12-15 11:07:55 +0100

    show owner of server in server list

--- modules/owner.py
+++ modules/owner.py
@@ -85,10 +85,12 @@ class Owner(commands.Cog):
     async def server_list(self, ctx):
         servers = ''
         user_count = ''
+        owner = ''
         server_ids = ''
         for guild in self.bot.guilds:
             servers += f'{guild.name}\n'
             user_count += f'{guild.member_count}\n'
+            owner += f'{guild.owner.mention}\n'
             server_ids += f'{guild.id}\n'
         embed = nextcord.Embed(
             title='Server Infos',
@@ -96,6 +98,7 @@ class Owner(commands.Cog):
         )
         embed.add_field(name='Name', value=servers, inline=True)
         embed.add_field(name='User count', value=user_count, inline=True)
+        embed.add_field(name='Owner', value=owner, inline=True)
         embed.add_field(name='Id', value=server_ids, inline=True)
         await ctx.send(embed=embed, delete_after=60)
 
