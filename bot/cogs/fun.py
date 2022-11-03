6b7ede6c2e7e8c0a6d73d2ec25eae1f44367d164
jackra1n <lok7ok@gmail.com>
2022-11-03 15:38:02 +0100

    Fix list formatting

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -153,26 +153,28 @@ class Fun(commands.Cog):
         """
         AKA 8ball, Ask the bot a question that you dont want the answer to.
         """
-        responses = ["It is certain.",
-                    "It is decidedly so.",
-                    "Without a doubt.",
-                    "Yes - definitely.",
-                    "You may rely on it.",
-                    "As I see it, yes.",
-                    "Most likely.",
-                    "Outlook good.",
-                    "Yes.",
-                    "Signs point to yes.",
-                    "Reply hazy, try again.",
-                    "Ask again later.",
-                    "Better not tell you now.",
-                    "Cannot predict now.",
-                    "Concentrate and ask again.",
-                    "Don't count on it.",
-                    "My reply is no.",
-                    "My sources say no.",
-                    "Outlook not so good.",
-                    "Very doubtful."]
+        responses = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        ]
         response = random.choice(responses)
         embed = discord.Embed(
             title=response,
