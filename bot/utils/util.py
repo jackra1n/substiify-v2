1245d49fe75832952b480dfbfe011d5e5d394717
jackra1n <lok7ok@gmail.com>
2022-11-09 20:16:53 +0100

    Improve readability, better centering

--- bot/utils/util.py
+++ bot/utils/util.py
@@ -15,24 +15,28 @@ def get_system_description() -> str:
 
 def print_system_info() -> None:
     system_label = get_colored("Running on:", colors.green, True)
-    python_version = get_colored("Python:", colors.cyan, True)
-    discord_version = get_colored("discord.py:", colors.yellow, True)
-    substiify_version = get_colored("substiify:", colors.red, True)
+    python_label = get_colored("Python:", colors.cyan, True)
+    discord_label = get_colored("discord.py:", colors.yellow, True)
+    substiify_label = get_colored("substiify:", colors.red, True)
 
     ascii_art = importlib.resources.read_text("utils", "art.txt")
     shell_width = os.get_terminal_size().columns
+    center_art = shell_width - (shell_width // 15)
     for line in ascii_art.splitlines():
-        print(line.center(shell_width))
+        print(line.center(center_art))
 
     system = get_system_description()
     system_length = len(system)
     longest_label = len(f'{system_label} {system}')
 
     rjust_len = (shell_width // 2) + (longest_label // 2)
+    python_version = platform.python_version().rjust(system_length)
+    discord_version = discord.__version__.rjust(system_length)
+    substiify_version = Version().get().rjust(system_length)
 
     print()
-    print(f'{system_label} {get_system_description()}'.rjust(rjust_len))
-    print(f'{python_version} {platform.python_version().rjust(system_length)}'.rjust(rjust_len))
-    print(f'{discord_version} {discord.__version__.rjust(system_length)}'.rjust(rjust_len))
-    print(f'{substiify_version} {Version().get().rjust(system_length)}'.rjust(rjust_len))
+    print(f'{system_label} {system}'.rjust(rjust_len))
+    print(f'{python_label} {python_version}'.rjust(rjust_len))
+    print(f'{discord_label} {discord_version}'.rjust(rjust_len))
+    print(f'{substiify_label} {substiify_version}'.rjust(rjust_len))
     print()
