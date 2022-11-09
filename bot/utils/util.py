92ce2ba4674baa1aa6059e27aa8b785f417ae503
jackra1n <lok7ok@gmail.com>
2022-11-09 18:17:33 +0100

    Improve system info placement, print substiify version

--- bot/utils/util.py
+++ bot/utils/util.py
@@ -4,6 +4,7 @@ import importlib.resources
 
 import discord
 from utils.colors import colors, get_colored
+from core.version import Version
 
 
 def get_system_description() -> str:
@@ -11,19 +12,27 @@ def get_system_description() -> str:
     filtered_system_bits = (s.strip() for s in system_bits if s.strip())
     return " ".join(filtered_system_bits)
 
+
 def print_system_info() -> None:
-    system_description = get_colored("Running on:", colors.green).ljust(30)
-    python_version = get_colored("Python:", colors.blue).ljust(30)
-    discord_version = get_colored("discord.py:", colors.yellow).ljust(30)
+    system_label = get_colored("Running on:", colors.green, True)
+    python_version = get_colored("Python:", colors.cyan, True)
+    discord_version = get_colored("discord.py:", colors.yellow, True)
+    substiify_version = get_colored("substiify:", colors.red, True)
 
     ascii_art = importlib.resources.read_text("utils", "art.txt")
     shell_width = os.get_terminal_size().columns
-
     for line in ascii_art.splitlines():
         print(line.center(shell_width))
 
+    system = get_system_description()
+    system_length = len(system)
+    longest_label = len(f'{system_label} {system}')
+
+    rjust_len = (shell_width // 2) + (longest_label // 2)
+
     print()
-    print(f'{system_description} {get_system_description()}'.center(shell_width))
-    print(f'{python_version} {platform.python_version()}'.center(shell_width))
-    print(f'{discord_version} {discord.__version__}'.center(shell_width))
+    print(f'{system_label} {get_system_description()}'.rjust(rjust_len))
+    print(f'{python_version} {platform.python_version().rjust(system_length)}'.rjust(rjust_len))
+    print(f'{discord_version} {discord.__version__.rjust(system_length)}'.rjust(rjust_len))
+    print(f'{substiify_version} {Version().get().rjust(system_length)}'.rjust(rjust_len))
     print()
