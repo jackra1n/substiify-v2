e8acf9890d85e91372388406709c1232f7685e25
jackra1n <45038833+jackra1n@users.noreply.github.com>
2025-08-28 21:56:41 +0200

    use version from VERSION file

--- core/__init__.py
+++ core/__init__.py
@@ -2,5 +2,11 @@ from . import config as config
 from . import constants as constants
 from .bot import Substiify as Substiify
 
-__version__ = "0.98"
+try:
+	from importlib.resources import files as _files
+	__version__ = _files(__package__) / "VERSION"
+	__version__ = __version__.read_text(encoding="utf-8").strip()
+except Exception:  # pragma: no cover
+	__version__ = "0.0.0"
+
 __author__ = "jackra1n"
